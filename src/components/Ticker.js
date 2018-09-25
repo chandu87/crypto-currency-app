f0a3515525913e389fd60cb1efe012fb85290479
chandu <chandrasr.palle@gmail.com>
2018-09-25 10:45:33 +0200

    crytocurrency component added

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Ticker.css";
+import Cryptocurrency from "../components/Cryptocurrency";
 
 class Ticker extends Component {
   constructor(props) {
@@ -8,31 +9,42 @@ class Ticker extends Component {
       data: [
         {
           id: "bitcoin",
-          price: "1"
+          name: "Bitcoin",
+          symbol: "BTC",
+          price_usd: "1",
+          percent_change_1h: "0",
+          percent_change_24h: "0",
+          percent_change_7d: "0"
         },
         {
           id: "ethereum",
-          price: "1"
+          name: "Ethereum",
+          symbol: "ETH",
+          price_usd: "1",
+          percent_change_1h: "0",
+          percent_change_24h: "0",
+          percent_change_7d: "0"
         },
         {
           id: "litecoin",
-          price: "1"
+          name: "Litecoin",
+          symbol: "LTC",
+          price_usd: "1",
+          percent_change_1h: "0",
+          percent_change_24h: "0",
+          percent_change_7d: "0"
         }
       ]
     };
   }
   render() {
-    let tickers = this.state.data.map(currency => (
-      <li key={currency.id}>
-        <h3>{currency.id}</h3>
-        <h4>{currency.price} USD</h4>
-      </li>
-    ));
-    return (<div className="tickers-container">
+    let tickers = this.state.data.map(currency => <Cryptocurrency data={currency} key={currency.id}/>);
+    return (
+      <div className="tickers-container">
         <ul className="tickers">{tickers}</ul>
         <p>Information updated every minute</p>
-
-    </div>);
+      </div>
+    );
   }
 }
 
